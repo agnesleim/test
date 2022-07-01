d8a4e4d2428a28255a220d22e5d6f6721f20004a
agnes <dvetelkin0@yandex.ru>
2022-07-01 18:31:21 +0300

    ready

---
+++ script.js
@@ -0,0 +1,12 @@
+const fon = document.getElementById("fon");
+const hlop = document.getElementById("hlop");
+
+document.addEventListener("keydown", function(event)
+{
+vzr();
+})
+
+function vzr ()
+{
+    hlop.classList.add("vzr")
+}
